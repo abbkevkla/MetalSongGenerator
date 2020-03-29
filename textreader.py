662b6ce935477e3bc4264d2e523d5bde19220bf1
abbkevkla <43879859+abbkevkla@users.noreply.github.com>
2020-03-29 18:55:08 +0200

    o

---
+++ textreader.py
@@ -0,0 +1,14 @@
+from comtypes.client import CreateObject
+from comtypes.gen import SpeechLib    
+engine = CreateObject("SAPI.SpVoice")
+stream = CreateObject("SAPI.SpFileStream")
+infile = "Small SLAYER.txt"
+outfile = "SLAYER-audio.mp3"
+stream.Open(outfile, SpeechLib.SSFMCreateForWrite)
+engine.AudioOutputStream = stream
+f = open(infile, 'r')
+theText = f.read()
+f.close()
+engine.speak(theText)
+stream.Close()
+
