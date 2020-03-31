2449a91aa8543cc53f61d008512db7d9d85d0192
abbkevkla <43879859+abbkevkla@users.noreply.github.com>
2020-03-31 13:09:48 +0300

    fixed tts

--- Results/Lyrics/textreader.py
+++ Results/Lyrics/textreader.py
@@ -1,9 +1,10 @@
-from comtypes.client import CreateObject
-from comtypes.gen import SpeechLib   
 # Denna kod fungerar endast för Windows 
+from comtypes.client import CreateObject  
 
 engine = CreateObject("SAPI.SpVoice")
 stream = CreateObject("SAPI.SpFileStream")
+from comtypes.gen import SpeechLib 
+
 infile = "SLAYER AI small 1.txt"
 outfile = "SLAYER AI small 1.wav" 
 # Jag testade att göra .mp3 filer på direkten, men filerna som skapades då fungerade inte qlls
